51949ceb74a94521b962d70d01d9a0051bf50fdd
Masatomo Yanagihara <masatomo.yanagihara.jp@gmail.com>
2020-10-25 19:19:18 +0900

    Add: MyClient.vueのテストコード

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,11 +3,15 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
+  // it('テストのタイトル', () => { テスト項目 })
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     })
+
+    // chai.jsが提供する検証用のメソッド
+    // 生成されたHTMLにmsg変数と同じテキスト（new message）が含まれているかを検証
     expect(wrapper.text()).to.include(msg)
   })
 })
